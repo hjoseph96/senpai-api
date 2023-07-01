b9d76af11fd4b5c33dbf8d8a4fcb89d452a9c0ac
Herbert Joseph <techniquea2z@gmail.com>
2023-06-30 23:44:08 -0400

    syntax

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :null_session
+    before_action :authorize_request
     
     def not_found
       render json: { error: 'not_found' }
@@ -14,11 +15,17 @@ class ApplicationController < ActionController::Base
         @current_user = User.find(@decoded[:user_id])
 
         render json: { status: 'access_denied' } if @current_user.nil?
+
+        render json: { user: @current_user }
       rescue ActiveRecord::RecordNotFound => e
         render json: { errors: e.message }, status: :unauthorized
       rescue JWT::DecodeError => e
         render json: { errors: e.message }, status: :unauthorized
       end
+    end
+
+    def verify_requests
+        @pending_requests =  VerifyRequest.where(status: :pending)
     end
   end
   
