618f0c15cefa03de006efde18a6af29d4c674b76
Herbert Joseph <techniquea2z@gmail.com>
2023-08-07 09:24:54 -0400

    Rank anime

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,6 +5,7 @@ module Types
     field :sign_in, mutation: Mutations::SignIn
     field :validate_phone, mutation: Mutations::ValidatePhone
     field :add_favorite_anime, mutation: Mutations::AddFavoriteAnime
+    field :rank_favorite_anime, mutation: Mutations::RankFavoriteAnime
     field :upload_photo, mutation: Mutations::UploadPhoto
     field :like_user, mutation: Mutations::LikeUser
     field :send_message, mutation: Mutations::SendMessage
