4c7a2c9e7a411aee49c8cbef76f148bb01ed7a6b
Herbert Joseph <techniquea2z@gmail.com>
2023-08-16 07:28:49 -0400

    Add anime selection to profile seeder

--- db/seeds/profile_seeder.rb
+++ db/seeds/profile_seeder.rb
@@ -47,44 +47,64 @@ class ProfileSeeder
             u.gallery.photos << photo
 
             u.save!
-        end
 
-        def create_males
-            puts 'Seeding men...'
-
-            ai_male_gallery_path = "#{Rails.root}/db/seeds/profile_seeds/male"
-            Dir.foreach(ai_male_gallery_path) do |filename|
-                next if filename == '.' or filename == '..'
-
-                u = User.create(
-                    phone: Faker::PhoneNumber.cell_phone,
-                    password: (SecureRandom.random_number(9e5) + 1e5).to_i,
-                    first_name: Faker::Name.male_first_name,
-                    role: :user,
-                    gender: :male,
-                    desired_gender: :desires_women,
-                    bio: Faker::Lorem.paragraphs,
-                    school: Faker::University.name,
-                    occupation: Faker::Job.position,
-                    current_sign_in_ip: '173.52.91.160',
-                    current_sign_in_at: DateTime.now
-                )
-
-                g = Gallery.create
-                u.gallery = g
-                u.save!    
-                
-                photo = File.open("#{ai_male_gallery_path}/#{filename}")
-                blob = ActiveStorage::Blob.create_and_upload!(
-                    io: photo,
-                    filename: filename
-                )
-                photo = Photo.new(order: 1)
-                photo.image.attach(blob)
-                u.gallery.photos << photo
-
-                u.save!
-            end
+            add_popular_anime(u)
         end
     end
+
+    def create_males
+        puts 'Seeding men...'
+
+        ai_male_gallery_path = "#{Rails.root}/db/seeds/profile_seeds/male"
+        Dir.foreach(ai_male_gallery_path) do |filename|
+            next if filename == '.' or filename == '..'
+
+            u = User.create(
+                phone: Faker::PhoneNumber.cell_phone,
+                password: (SecureRandom.random_number(9e5) + 1e5).to_i,
+                first_name: Faker::Name.male_first_name,
+                role: :user,
+                gender: :male,
+                desired_gender: :desires_women,
+                bio: Faker::Lorem.paragraphs,
+                school: Faker::University.name,
+                occupation: Faker::Job.position,
+                current_sign_in_ip: '173.52.91.160',
+                current_sign_in_at: DateTime.now
+            )
+
+            g = Gallery.create
+            u.gallery = g
+            u.save!    
+            
+            photo = File.open("#{ai_male_gallery_path}/#{filename}")
+            blob = ActiveStorage::Blob.create_and_upload!(
+                io: photo,
+                filename: filename
+            )
+            photo = Photo.new(order: 1)
+            photo.image.attach(blob)
+            u.gallery.photos << photo
+
+            u.save!
+
+            add_popular_anime(u)
+        end
+    end
+
+    def add_popular_anime(user)
+        chosen_anime = []
+
+        anime = Anime.order(popularity: :asc).reverse_order.limit(100).reverse
+
+        10.times do
+            random_anime = anime.sample
+
+            chosen_anime << random_anime unless chosen_anime.include? random_anime
+        end
+
+        user.animes = chosen_anime
+        user.save!
+    end
+
 end
