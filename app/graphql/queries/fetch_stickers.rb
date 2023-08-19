9e48a41c997da27327109ce35d37ebae77b2438d
Herbert Joseph <techniquea2z@gmail.com>
2023-08-18 22:59:00 -0400

    Add FetchStickers query

--- app/graphql/queries/fetch_stickers.rb
+++ app/graphql/queries/fetch_stickers.rb
@@ -6,11 +6,9 @@ module Queries
       type [Types::StickerType], null: false
   
       def resolve(page:)
-        anime_params = Hash params
-
         p = page || 1
 
-        results = Sticker.all.page(page).per(30)
+        results = Sticker.all.page(p).per(30)
 
         results
       end
