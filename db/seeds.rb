b77b308a770260762aa993602549cb8fad3fd572
Herbert Joseph <techniquea2z@gmail.com>
2023-08-19 12:18:20 -0400

    refactor feed for scale

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ require_relative './seeds/anilist_seeder'
 require_relative './seeds/profile_seeder'
 require_relative './seeds/sticker_seeder'
 
-# AnilistSeeder.create_animes
-3077.times {|i| puts i; ProfileSeeder.create_profiles }
-# StickerSeeder.create_stickers
+AnilistSeeder.create_animes
+ProfileSeeder.create_profiles
+StickerSeeder.create_stickers
