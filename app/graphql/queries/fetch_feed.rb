055f6ab82c2b5b4ad0be9af4118161d5de641cb7
Herbert Joseph <techniquea2z@gmail.com>
2023-08-20 09:57:40 -0400

    Add mile distance

--- app/graphql/queries/fetch_feed.rb
+++ app/graphql/queries/fetch_feed.rb
@@ -3,24 +3,23 @@ module Queries
         graphql_name "FetchFeed"
 
         argument :user_id, ID, required: true
+        argument :miles_away, Integer, required: true
         argument :page, Integer, required: false
 
         type [Types::UserType], null: false
 
-        def resolve(user_id:, page:)
-          page = page || 1
-
+        def resolve(user_id:, miles_away:, page: 1)
           feed = Rails.cache.read("#{user_id}-FEED")
 
           if feed.present?
             results = User.where(id: feed)
           else
-            results = FeedLoader.create_feed(user_id: user_id)
+            results = FeedLoader.create_feed(user_id: user_id, distance_in_miles: miles_away)
 
             Rails.cache.write("#{user_id}-FEED", results.pluck(:id))
           end
 
-          results.page(page)
+          results.page(page).per(25)
         end
     end
 end
