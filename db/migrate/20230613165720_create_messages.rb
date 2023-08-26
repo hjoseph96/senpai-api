dda97af1e30c09a5ffe7384a7329adec9ed69c6e
Herbert Joseph <techniquea2z@gmail.com>
2023-08-26 03:11:03 -0400

    Bundle update

--- db/migrate/20230613165720_create_messages.rb
+++ db/migrate/20230613165720_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
-      t.integer :sender_id
-      t.text :content
+      t.integer :sender_id, null: false
+      t.text :content, null: false
       t.integer :reaction
       
       t.timestamps
