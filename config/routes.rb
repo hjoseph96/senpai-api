cc61ea53457284f08d298d0bdaa84655190eb550
Herbert Joseph <herbertjoseph@Herberts-MacBook-Pro.local>
2023-11-28 12:44:31 -0500

    Makee deleteFavoriteAnnime to take array of ianime_ds

--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,12 @@ Rails.application.routes.draw do
 
   namespace :v1 do
     namespace :admin do
-
       resources :users do
         collection do
           get '/all_users', to: 'users#all_users'
         end
 
-        post '/ban_user', to: 'users#ban_user', as: :ban_user
+        post '/ba2n_user', to: 'users#ban_user', as: :ban_user
         post '/warn_user', to: 'users#warn_user', as: :warn_user
       end
 
