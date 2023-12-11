b151f0704abaf70a10c1e77356dc72a2772735c8
Herbert Joseph <techniquea2z@gmail.com>
2023-12-11 10:06:49 -0500

    Allow array for addFavoriteMusic

--- app/graphql/mutations/add_favorite_music.rb
+++ app/graphql/mutations/add_favorite_music.rb
@@ -4,19 +4,21 @@ module Mutations
   class AddFavoriteMusic < Mutations::BaseMutation
     graphql_name "AddFavoriteMusic"
 
-    argument :params, Types::Input::FavoriteMusicInputType, required: true
+    argument :params, [Types::Input::FavoriteMusicInputType], required: true
 
     field :user, Types::UserType, null: false
     def resolve(params:)
-      @current_user = User.find(params[:user_id])
+      @current_user = User.find(params[0][:user_id])
 
       begin
-        FavoriteMusic.create!(
-          user_id: @current_user.id,
-          music_type: params[:music_type],
-          cover_url: params[:cover_url],
-          name: params[:name]
-        )
+        params.each do |p|
+          FavoriteMusic.create!(
+            user_id: @current_user.id,
+            music_type: p[:music_type],
+            cover_url: p[:cover_url],
+            name: p[:name]
+          )
+        end
 
         { user: @current_user.reload }
       rescue ActiveRecord::RecordInvalid => e
