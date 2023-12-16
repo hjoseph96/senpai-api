72da9e6d7e99c7651fa19679a7af600b30f32352
Herbert Joseph <techniquea2z@gmail.com>
2023-12-16 13:20:33 -0500

    Message#attachment size validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,10 +3,12 @@ class Message < ApplicationRecord
     belongs_to :sender, class_name: :User, foreign_key: 'sender_id'
     belongs_to :sticker, required: false
     has_one :recommendation, required: false
-  
+
+    has_one_attached :attachment, required: false
+
+    validates :attachment, attached: true,  size: { less_than: 50.megabytes , message: 'is too large' }
     validates_presence_of :content
 
-    has_one_attached :attachment
 
     enum :reaction, %w(funny like heart vomit angry demon)
   
