c799204452aac5def7c688b1090c7b7050d51f80
Herbert Joseph <techniquea2z@gmail.com>
2023-12-16 15:09:42 -0500

    Add gem

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
     include ActiveStorageValidations
-    
+
     belongs_to :conversation
     belongs_to :sender, class_name: :User, foreign_key: 'sender_id'
     belongs_to :sticker, required: false
