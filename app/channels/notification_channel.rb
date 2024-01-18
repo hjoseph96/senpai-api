aa7b0bd99690d6ba3d2cb72fb4f428f12544a80e
Herbert Joseph <techniquea2z@gmail.com>
2024-01-18 14:43:22 -0500

    NotificationChannel debugging

--- app/channels/notification_channel.rb
+++ app/channels/notification_channel.rb
@@ -1,5 +1,9 @@
 class NotificationChannel < ApplicationCable::Channel
   def subscribed
+    puts "====================================="
+    puts "NOTIFICATIONS FOR #{current_user.id}"
+    puts '====================================='
+    
     stream_from "Notification-#{current_user.id}"
   end
 
