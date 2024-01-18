d01f4dd064d805b15a307e59016da6333398c392
Herbert Joseph <techniquea2z@gmail.com>
2024-01-18 16:15:04 -0500

    Add fixes to message websockets

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,9 +2,8 @@ class RoomChannel < ApplicationCable::Channel
     # calls when a client connects to the server
     def subscribed
       if params[:room_id].present?
-        stream_from("ChatRoom-#{(params[:room_id])}")
+        stream_from("ChatRoom-#{params[:room_id]}")
       end
-
     end
 
     def unsubscribed
