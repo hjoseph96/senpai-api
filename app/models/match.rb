629ce43d5aec0f781bec50079f246cc9d9cb17f0
Herbert Joseph <techniquea2z@gmail.com>
2024-01-26 21:17:58 -0500

    Fix report deletion

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,5 +3,5 @@ class Match < ApplicationRecord
 
   belongs_to :user
   belongs_to :matchee, foreign_key: :matchee_id, class_name: 'User'
-  has_one :conversation, dependent: :destroy
+  has_one :conversation
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -2,6 +2,7 @@ class Report < ApplicationRecord
   belongs_to :user
   belongs_to :offender, foreign_key: :offense_id, class_name: 'User'
   belongs_to :conversation
+  has_one :block, dependent: :destroy
 
   enum :status, [:open, :resolved]
   enum :reason, [:inappropriate_behavior, :spam, :sexual_abuse, :doxxing]
