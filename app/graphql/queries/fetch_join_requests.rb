b3822c8b279f8945121481c62a0993b3ff541e0a
Herbert Joseph <techniquea2z@gmail.com>
2024-03-25 15:51:32 -0400

    Add applicant_id

--- app/graphql/queries/fetch_join_requests.rb
+++ app/graphql/queries/fetch_join_requests.rb
@@ -3,14 +3,19 @@ module Queries
     graphql_name "FetchJoinRequests"
 
     argument :event_id, ID, required: true
+    argument :applicant_id, ID, required: false
     argument :page, Integer, required: false
 
     type [Types::JoinRequestType], null: false
 
-    def resolve(event_id:, page: 1)
+    def resolve(event_id:, page: 1, applicant_id: nil)
       results = JoinRequest.includes(:event).where(events: { id: event_id }).where(status: :pending)
 
       results = results.order(created_at: :desc)
+
+      unless applicant_id.nil?
+        results = results.where(user_id: applicant_id)
+      end
 
       results.page(page)
     end
