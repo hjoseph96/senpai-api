3c9342dd1e8451f564c49dfce8d72c4ef5d09d1a
Herbert Joseph <techniquea2z@gmail.com>
2024-03-31 05:52:08 -0400

    Fix advanceToNextRound

--- db/migrate/20240331094720_default_completed_false_for_tournaments.rb
+++ db/migrate/20240331094720_default_completed_false_for_tournaments.rb
@@ -1,6 +1,5 @@
 class DefaultCompletedFalseForTournaments < ActiveRecord::Migration[7.1]
   def change
-    Tournament.update_all(completed: false)
     change_column :tournaments, :completed, :boolean, default: false, null: false
   end
 end
