9b2bfa513c8623ddc47d71bfc38f105141a9aa23
Herbert Joseph <techniquea2z@gmail.com>
2024-04-10 13:16:10 -0400

    Do not require start_date

--- app/graphql/types/input/search_event_input_type.rb
+++ app/graphql/types/input/search_event_input_type.rb
@@ -4,7 +4,7 @@ module Types
       argument :user_id, ID, required: false
       argument :query, String, required: false
       argument :miles_away, Integer, required: false
-      argument :start_date, GraphQL::Types::ISO8601DateTime, required: true
+      argument :start_date, GraphQL::Types::ISO8601DateTime, required: false
       argument :end_date, GraphQL::Types::ISO8601DateTime, required: false
       argument :payment_required, Boolean, required: false
       argument :cosplay_required, String, required: false
