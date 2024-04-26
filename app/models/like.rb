529775b6840f7f8409ffa75da14706c367973e60
Herbert Joseph <techniquea2z@gmail.com>
2024-04-26 13:43:22 -0400

    Add likee param

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   acts_as_paranoid
 
   belongs_to :user
-  has_one :likee
+  belongs_to :likee, foreign_key: :likee_id, class_name: 'User'
 
   enum :like_type, [ :standard, :super, :rejection ]
 end
